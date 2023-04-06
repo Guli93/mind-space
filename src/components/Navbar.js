527798bdd0c7dc511cd42f330e61948bc9d233e8
Gulista <gulistasiddiqui5@gmail.com>
2023-04-06 13:23:52 +0530

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from '../images/logo.png';
 import { Link } from 'react-router-dom';
+import { FaBars ,FaCross} from 'react-icons/fa';
+
 
 const Navbar = () => {
     return (
@@ -21,6 +23,11 @@ const Navbar = () => {
                             <Link to='/login'>Login/Signup</Link>
                         </li>
                     </ul>
+                </div>
+
+                <div className='mobile'>
+                    <FaBars className='ham'/>
+                    <FaCross className='cross'/>
                 </div>
             </nav>
         </>
